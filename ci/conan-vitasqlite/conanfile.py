bbb3ea643490eb70130d99c034752b952da6717c
Philippe Daouadi <p.daouadi@free.fr>
2018-06-24 14:42:03 +0200

    conan-vitasqlite: add patch for temp file support

--- ci/conan-vitasqlite/conanfile.py
+++ ci/conan-vitasqlite/conanfile.py
@@ -8,12 +8,14 @@ class SqliteConan(ConanFile):
     name = "vitasqlite"
     version = "0.0.1"
     settings = "os", "compiler", "build_type", "arch"
+    exports_sources = "temp-support.patch"
 
     def source(self):
         source_url = "https://github.com/VitaSmith/libsqlite"
         commit = "7bf41a937d0358a1f0740950b30e8444ca8beea0"
         self.run("git clone %s" % source_url)
         self.run("git checkout %s" % commit, cwd="libsqlite")
+        tools.patch(base_path="libsqlite", patch_file="temp-support.patch", strip=0)
 
         tools.download("https://www.sqlite.org/2018/sqlite-amalgamation-3230100.zip", "sqlite.zip")
         tools.unzip("sqlite.zip")
