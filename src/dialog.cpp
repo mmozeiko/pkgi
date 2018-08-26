73a73030bb0fd3266d775f11534156eec0017f1d
Philippe Daouadi <p.daouadi@free.fr>
2018-08-26 12:18:21 +0200

    dialog: use more std::string

--- src/dialog.cpp
+++ src/dialog.cpp
@@ -7,6 +7,8 @@ extern "C"
 }
 #include "pkgi.hpp"
 
+namespace
+{
 typedef enum
 {
     DialogNone,
@@ -14,29 +16,30 @@ typedef enum
     DialogError,
 } DialogType;
 
-static DialogType dialog_type;
-static char dialog_title[256];
-static char dialog_text[256];
-static char dialog_extra[256];
-static int dialog_allow_close;
-static int dialog_cancelled;
+DialogType dialog_type;
+std::string dialog_title;
+std::string dialog_text;
+std::string dialog_extra;
+int dialog_allow_close;
+int dialog_cancelled;
 
-static int32_t dialog_width;
-static int32_t dialog_height;
-static int32_t dialog_delta;
+int32_t dialog_width;
+int32_t dialog_height;
+int32_t dialog_delta;
+}
 
-void pkgi_dialog_init(void)
+void pkgi_dialog_init()
 {
     dialog_type = DialogNone;
     dialog_allow_close = 1;
 }
 
-int pkgi_dialog_is_open(void)
+int pkgi_dialog_is_open()
 {
     return dialog_type != DialogNone;
 }
 
-int pkgi_dialog_is_cancelled(void)
+int pkgi_dialog_is_cancelled()
 {
     return dialog_cancelled;
 }
@@ -52,9 +55,9 @@ void pkgi_dialog_message(const char* text)
 {
     pkgi_dialog_lock();
 
-    pkgi_strncpy(dialog_text, sizeof(dialog_text), text);
-    dialog_title[0] = 0;
-    dialog_extra[0] = 0;
+    dialog_text = text;
+    dialog_title = "";
+    dialog_extra = "";
 
     dialog_cancelled = 0;
     dialog_type = DialogMessage;
@@ -69,9 +72,9 @@ void pkgi_dialog_error(const char* text)
 
     pkgi_dialog_lock();
 
-    pkgi_strncpy(dialog_title, sizeof(dialog_title), "ERROR");
-    pkgi_strncpy(dialog_text, sizeof(dialog_text), text);
-    dialog_extra[0] = 0;
+    dialog_title = "ERROR";
+    dialog_text = text;
+    dialog_extra = "";
 
     dialog_cancelled = 0;
     dialog_type = DialogError;
@@ -80,7 +83,7 @@ void pkgi_dialog_error(const char* text)
     pkgi_dialog_unlock();
 }
 
-void pkgi_dialog_close(void)
+void pkgi_dialog_close()
 {
     dialog_delta = -1;
 }
@@ -110,7 +113,7 @@ void pkgi_do_dialog(pkgi_input* input)
         if (dialog_delta < 0 && (dialog_width <= 0 || dialog_height <= 0))
         {
             dialog_type = DialogNone;
-            dialog_text[0] = 0;
+            dialog_text = "";
             dialog_extra[0] = 0;
 
             dialog_width = 0;
@@ -132,16 +135,16 @@ void pkgi_do_dialog(pkgi_input* input)
     }
 
     DialogType local_type = dialog_type;
-    char local_title[256];
-    char local_text[256];
-    char local_extra[256];
+    std::string local_title;
+    std::string local_text;
+    std::string local_extra;
     int local_allow_close = dialog_allow_close;
     int32_t local_width = dialog_width;
     int32_t local_height = dialog_height;
 
-    pkgi_strncpy(local_title, sizeof(local_title), dialog_title);
-    pkgi_strncpy(local_text, sizeof(local_text), dialog_text);
-    pkgi_strncpy(local_extra, sizeof(local_extra), dialog_extra);
+    local_title = dialog_title;
+    local_text = dialog_text;
+    local_extra = dialog_extra;
 
     pkgi_dialog_unlock();
 
@@ -165,7 +168,7 @@ void pkgi_do_dialog(pkgi_input* input)
     int w = VITA_WIDTH - 2 * PKGI_DIALOG_HMARGIN;
     int h = VITA_HEIGHT - 2 * PKGI_DIALOG_VMARGIN;
 
-    if (local_title[0])
+    if (!local_title.empty())
     {
         uint32_t color;
         if (local_type == DialogError)
@@ -177,7 +180,7 @@ void pkgi_do_dialog(pkgi_input* input)
             color = PKGI_COLOR_TEXT_DIALOG;
         }
 
-        int width = pkgi_text_width(local_title);
+        int width = pkgi_text_width(local_title.c_str());
         if (width > w + 2 * PKGI_DIALOG_PADDING)
         {
             pkgi_clip_set(
@@ -189,7 +192,7 @@ void pkgi_do_dialog(pkgi_input* input)
                     (VITA_WIDTH - width) / 2,
                     PKGI_DIALOG_VMARGIN + font_height,
                     color,
-                    local_title);
+                    local_title.c_str());
             pkgi_clip_remove();
         }
         else
@@ -198,7 +201,7 @@ void pkgi_do_dialog(pkgi_input* input)
                     (VITA_WIDTH - width) / 2,
                     PKGI_DIALOG_VMARGIN + font_height,
                     color,
-                    local_title);
+                    local_title.c_str());
         }
     }
 
@@ -212,7 +215,7 @@ void pkgi_do_dialog(pkgi_input* input)
         color = PKGI_COLOR_TEXT_ERROR;
     }
 
-    int textw = pkgi_text_width(local_text);
+    int textw = pkgi_text_width(local_text.c_str());
     if (textw > w + 2 * PKGI_DIALOG_PADDING)
     {
         pkgi_clip_set(
@@ -224,7 +227,7 @@ void pkgi_do_dialog(pkgi_input* input)
                 PKGI_DIALOG_HMARGIN + PKGI_DIALOG_PADDING,
                 VITA_HEIGHT / 2 - font_height / 2,
                 color,
-                local_text);
+                local_text.c_str());
         pkgi_clip_remove();
     }
     else
@@ -233,21 +236,18 @@ void pkgi_do_dialog(pkgi_input* input)
                 (VITA_WIDTH - textw) / 2,
                 VITA_HEIGHT / 2 - font_height / 2,
                 color,
-                local_text);
+                local_text.c_str());
     }
 
     if (local_allow_close)
     {
-        char text[256];
-        pkgi_snprintf(
-                text,
-                sizeof(text),
-                "press %s to close",
+        std::string text = fmt::format(
+                "press {} to close",
                 pkgi_ok_button() == PKGI_BUTTON_X ? PKGI_UTF8_X : PKGI_UTF8_O);
         pkgi_draw_text(
-                (VITA_WIDTH - pkgi_text_width(text)) / 2,
+                (VITA_WIDTH - pkgi_text_width(text.c_str())) / 2,
                 PKGI_DIALOG_VMARGIN + h - 2 * font_height,
                 PKGI_COLOR_TEXT_DIALOG,
-                text);
+                text.c_str());
     }
 }
