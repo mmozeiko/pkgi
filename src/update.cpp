540b14207bbd9d5ee6f9fd718bc8f61eccdcb1b5
Philippe Daouadi <p.daouadi@free.fr>
2018-09-14 23:01:43 +0200

    update: catch update errors

--- src/update.cpp
+++ src/update.cpp
@@ -15,47 +15,56 @@ std::string version;
 
 void start_download()
 {
-    LOGF("starting downloading version {}", version);
-
-    const auto filename =
-            fmt::format("{}/pkgj-v{}.vpk", pkgi_get_config_folder(), version);
-    const auto url = fmt::format("{}/pkgj-v{}.vpk", PKGJ_UPDATE_URL, version);
-
-    pkgi_dialog_message("Downloading update", 0);
-
     try
     {
-        const auto file = pkgi_create(filename.c_str());
-        BOOST_SCOPE_EXIT_ALL(&)
-        {
-            pkgi_close(file);
-        };
+        LOGF("starting downloading version {}", version);
 
-        VitaHttp http;
-        http.start(url, 0);
-        std::vector<uint8_t> data(64 * 1024);
-        while (true)
+        const auto filename = fmt::format(
+                "{}/pkgj-v{}.vpk", pkgi_get_config_folder(), version);
+        const auto url =
+                fmt::format("{}/pkgj-v{}.vpk", PKGJ_UPDATE_URL, version);
+
+        pkgi_dialog_message("Downloading update", 0);
+
+        try
         {
-            const auto read = http.read(data.data(), data.size());
-            if (read == 0)
-                break;
-            pkgi_write(file, data.data(), read);
+            const auto file = pkgi_create(filename.c_str());
+            BOOST_SCOPE_EXIT_ALL(&)
+            {
+                pkgi_close(file);
+            };
+
+            VitaHttp http;
+            http.start(url, 0);
+            std::vector<uint8_t> data(64 * 1024);
+            while (true)
+            {
+                const auto read = http.read(data.data(), data.size());
+                if (read == 0)
+                    break;
+                pkgi_write(file, data.data(), read);
+            }
+
+            LOGF("update download complete");
+        }
+        catch (...)
+        {
+            LOGF("download failed, removing partial file");
+            pkgi_rm(filename.c_str());
+            throw;
         }
 
-        LOGF("update download complete");
+        pkgi_dialog_message(
+                fmt::format(
+                        "The update has been downloaded to {}, install "
+                        "it through VitaShell.",
+                        filename)
+                        .c_str());
     }
-    catch (...)
+    catch (const std::exception& e)
     {
-        LOGF("download failed, removing partial file");
-        pkgi_rm(filename.c_str());
-        throw;
+        pkgi_dialog_error(fmt::format("Download failed: {}", e.what()).c_str());
     }
-
-    pkgi_dialog_message(fmt::format(
-                                "The update has been downloaded to {}, install "
-                                "it through VitaShell.",
-                                filename)
-                                .c_str());
 }
 
 void update_thread()
