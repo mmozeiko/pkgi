3230e2636b424ef956cb12dfae64625493e7f14e
Philippe Daouadi <p.daouadi@free.fr>
2018-09-16 19:43:06 +0200

    Upgrade vitasdk to patched version to fix random crash
    Closes #200

--- ci/conan-vitasdk/conanfile.py
+++ ci/conan-vitasdk/conanfile.py
@@ -5,14 +5,14 @@ from conans import ConanFile, tools
 
 class VitasdkToolchainConan(ConanFile):
     name = "vitasdk-toolchain"
-    lib_version = "06"
+    lib_version = "07"
     package_version = ""
     exports_sources = "cmake-toolchain.patch"
     version = "%s%s" % (lib_version, package_version)
     settings = "os", "arch"
 
     def source(self):
-        tools.download("https://github.com/blastrock/autobuilds/releases/download/master-linux-v6/vitasdk-x86_64-linux-gnu-2018-06-06_16-34-29.tar.bz2", filename="vitasdk.tar.bz2")
+        tools.download("https://github.com/blastrock/autobuilds/releases/download/master-linux-v7/vitasdk-x86_64-linux-gnu-2018-09-16_17-17-04.tar.bz2", filename="vitasdk.tar.bz2")
         tools.untargz("vitasdk.tar.bz2")
 
         tools.patch(base_path="vitasdk", patch_file="cmake-toolchain.patch", strip=1)
