48d693e57d902e978c958ea9fd75bf9c5b2f9557
Philippe Daouadi <p.daouadi@free.fr>
2018-12-16 11:24:27 +0100

    sha256: add hmac function

--- src/sha256.cpp
+++ src/sha256.cpp
@@ -1,6 +1,7 @@
 #include "sha256.hpp"
 
-#include <string.h>
+#include <cstring>
+#include <stdexcept>
 
 #if __ARM_NEON__
 
@@ -313,4 +314,128 @@ void sha256_finish(sha256_ctx* ctx, uint8_t* digest)
     {
         set32be(digest + 4 * i, ctx->state[i]);
     }
+}
+
+/**
+ * sha256_vector - SHA256 hash for data vector
+ * @num_elem: Number of elements in the data vector
+ * @addr: Pointers to the data areas
+ * @len: Lengths of the data blocks
+ * @mac: Buffer for the hash
+ *
+ * From vitasdk
+ */
+void sha256_vector(
+        size_t num_elem, const uint8_t* addr[], const size_t* len, uint8_t* mac)
+{
+    sha256_ctx ctx;
+    size_t i;
+
+    sha256_init(&ctx);
+    for (i = 0; i < num_elem; i++)
+        sha256_update(&ctx, addr[i], len[i]);
+    sha256_finish(&ctx, mac);
+}
+
+/**
+ * hmac_sha256_vector - HMAC-SHA256 over data vector (RFC 2104)
+ * @key: Key for HMAC operations
+ * @key_len: Length of the key in bytes
+ * @num_elem: Number of elements in the data vector
+ * @addr: Pointers to the data areas
+ * @len: Lengths of the data blocks
+ * @mac: Buffer for the hash (32 bytes)
+ *
+ * From vitasdk
+ */
+void hmac_sha256_vector(
+        const uint8_t* key,
+        size_t key_len,
+        size_t num_elem,
+        const uint8_t* addr[],
+        const size_t* len,
+        uint8_t* mac)
+{
+    unsigned char k_pad[64]; /* padding - key XORd with ipad/opad */
+    unsigned char tk[32];
+    const uint8_t* _addr[6];
+    size_t _len[6], i;
+
+    if (num_elem > 5)
+    {
+        /*
+         * Fixed limit on the number of fragments to avoid having to
+         * allocate memory (which could fail).
+         */
+        throw std::runtime_error("Too many parts for HMAC-SHA256");
+    }
+
+    /* if key is longer than 64 bytes reset it to key = SHA256(key) */
+    if (key_len > 64)
+    {
+        sha256_vector(1, &key, &key_len, tk);
+        key = tk;
+        key_len = 32;
+    }
+
+    /* the HMAC_SHA256 transform looks like:
+     *
+     * SHA256(K XOR opad, SHA256(K XOR ipad, text))
+     *
+     * where K is an n byte key
+     * ipad is the byte 0x36 repeated 64 times
+     * opad is the byte 0x5c repeated 64 times
+     * and text is the data being protected */
+
+    /* start out by storing key in ipad */
+    memset(k_pad, 0, sizeof(k_pad));
+    memcpy(k_pad, key, key_len);
+    /* XOR key with ipad values */
+    for (i = 0; i < 64; i++)
+        k_pad[i] ^= 0x36;
+
+    /* perform inner SHA256 */
+    _addr[0] = k_pad;
+    _len[0] = 64;
+    for (i = 0; i < num_elem; i++)
+    {
+        _addr[i + 1] = addr[i];
+        _len[i + 1] = len[i];
+    }
+    sha256_vector(1 + num_elem, _addr, _len, mac);
+
+    // NOTE: SCE HACK - they removed the clearing of the pad in their version.
+    // memset(k_pad, 0, sizeof(k_pad));
+    // memcpy(k_pad, key, key_len);
+    /* XOR key with opad values */
+    for (i = 0; i < 64; i++)
+        // NOTE: SCE HACK - they changed the normal 0x5C value to 0x6A
+        k_pad[i] ^= 0x6A;
+
+    /* perform outer SHA256 */
+    _addr[0] = k_pad;
+    _len[0] = 64;
+    _addr[1] = mac;
+    _len[1] = SHA256_MAC_LEN;
+    sha256_vector(2, _addr, _len, mac);
+}
+
+/**
+ * hmac_sha256 - HMAC-SHA256 over data buffer (RFC 2104)
+ * @key: Key for HMAC operations
+ * @key_len: Length of the key in bytes
+ * @data: Pointers to the data area
+ * @data_len: Length of the data area
+ * @mac: Buffer for the hash (20 bytes)
+ *
+ * From vitasdk
+ */
+void hmac_sha256(
+        const uint8_t* key,
+        size_t key_len,
+        const uint8_t* data,
+        size_t data_len,
+        uint8_t* mac)
+{
+    hmac_sha256_vector(key, key_len, 1, &data, &data_len, mac);
 }
--- src/sha256.hpp
+++ src/sha256.hpp
@@ -4,9 +4,11 @@ extern "C"
 {
 #include "utils.h"
 }
+#include <cstddef>
 
 #define SHA256_BLOCK_SIZE 64
 #define SHA256_DIGEST_SIZE 32
+#define SHA256_MAC_LEN 32
 
 typedef struct
 {
@@ -18,3 +20,21 @@ typedef struct
 void sha256_init(sha256_ctx* ctx);
 void sha256_update(sha256_ctx* ctx, const uint8_t* buffer, uint32_t size);
 void sha256_finish(sha256_ctx* ctx, uint8_t* digest);
+void sha256_vector(
+        size_t num_elem,
+        const uint8_t* addr[],
+        const size_t* len,
+        uint8_t* mac);
+void hmac_sha256_vector(
+        const uint8_t* key,
+        size_t key_len,
+        size_t num_elem,
+        const uint8_t* addr[],
+        const size_t* len,
+        uint8_t* mac);
+void hmac_sha256(
+        const uint8_t* key,
+        size_t key_len,
+        const uint8_t* data,
+        size_t data_len,
+        uint8_t* mac);
