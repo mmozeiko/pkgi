53cd84dd2f38ae336788970221f891d79b2c12dd
cuevavirus <53134627+cuevavirus@users.noreply.github.com>
2020-01-04 18:41:08 -0500

    Compress menu entries to fit all on screen

--- src/menu.cpp
+++ src/menu.cpp
@@ -261,7 +261,7 @@ int pkgi_do_menu(pkgi_input* input)
         MenuType type = entry->type;
         if (type == MenuText)
         {
-            y += font_height / 2;
+            y += font_height / 3;
         }
         else if (type == MenuSearchClear && !menu_search_clear)
         {
@@ -271,8 +271,6 @@ int pkgi_do_menu(pkgi_input* input)
             y += font_height / 3;
         else if (type == MenuShow)
         {
-            if (entry[-1].type != MenuShow)
-                y += font_height / 3;
             if (!(entry->value & menu_allow_refresh))
             {
                 continue;
@@ -321,7 +319,7 @@ int pkgi_do_menu(pkgi_input* input)
         }
         pkgi_draw_text(x, y, color, text);
 
-        y += font_height;
+        y += font_height - 1;
     }
 
     return 1;
